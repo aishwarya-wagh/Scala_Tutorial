fe2265987666bd6c150bffae54507d1cd73befb2
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-07-02 18:58:59 -0400

    basic control structures

---
+++ src/Control_Structure/IfElseExample.scala
@@ -0,0 +1,13 @@
+package Control_Structure
+
+object IfElseExample extends App {
+  val x = 10
+
+  if (x > 0) {
+    println("x is positive")
+  } else if (x < 0) {
+    println("x is negative")
+  } else {
+    println("x is zero")
+  }
+}
